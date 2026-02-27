acafb2e003b5dea7f700631be7bd4ba79bec3a64
plbertonm <plbertonm@gmail.com>
2026-02-26 22:11:13 -0300

    Neovim Dotfiles V2 (which-key quase pronto)

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,7 +1,31 @@
 return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {},
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "helix", -- helix=No canto direito, classic=clássico, modern=classico bonito como helix 
+    plugins = {
+      marks = false,     -- desativa o menu de marks ao digitar ' ou `
+      registers = false, -- desativa o menu de registros ao digitar "
+      spelling = { enabled = false }, -- desativa o z=
+      -- Na v3, operadores (como d, c, y, v) também são controlados aqui
+    presets = {
+      operators = false,    -- isso impede que apareça ajuda ao clicar v, d, y, etc.
+      motions = false,      -- desativa ajuda para h, j, k, l...
+      text_objects = false, -- desativa ajuda para iw, ap...
+      windows = false,      -- desativa ajuda para <C-w>
+      nav = false,          -- desativa ajuda para g...
+      z = false,            -- desativa ajuda para comandos com z
+      g = false,            -- desativa ajuda para comandos com g
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
   },
 }
