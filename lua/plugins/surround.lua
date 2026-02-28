9c9ede31b8565f212a094af116a733c1717aaccf
plbertonm <plbertonm@gmail.com>
2026-02-28 20:51:39 -0300

    Integração quase perfeita com Mini
    Adicionados novos plugins:
    	mini.ai
    	mini.comment
    	mini.completion
    	mini.files
    	mini.icons
    	mini.move
    	mini.pick
    	mini.snippets
    	mini.splitjoin
    	mini.starter
    	mini.surround
    
    Plugins removidos:
    	nvim.treesitter (removido para tester)
    	blink.cmp
    
    Plugins modificados:
    	nvim.which-key
    
    Adicionados novos keymaps

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,16 @@
+return {
+  "echasnovski/mini.surround",
+  event = "VeryLazy",
+  config = function ()
+    require('mini.surround').setup({
+      mappings = {
+        add = 'sa', -- Add surrounding in Normal and Visual modes
+        delete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+      },
+    })
+  end,
+} 
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,22 +4,6 @@ return {
   opts = {
     preset = "helix", -- helix = No canto direito, classic = clássico (em baixo, grande), modern = clássico bonito, como helix 
     icons = { mappings = false },
-    plugins = {
-      marks = false,     -- desativa o menu de marks ao digitar 'ou'
-      registers = false, -- desativa o menu de registros ao digitar "
-      spelling = { enabled = false }, -- desativa o z
-    presets = {
-      operators = false,    -- isso impede que apareça ajuda ao clicar v, d, y, etc.
-      motions = false,      -- desativa ajuda para h, j, k, l...
-      text_objects = false, -- desativa ajuda para iw, ap...
-      windows = false,      -- desativa ajuda para <C-w>
-      nav = false,          -- desativa ajuda para g...
-      z = false,            -- desativa ajuda para comandos com z
-      g = false,            -- desativa ajuda para comandos com g
-      },
-    },
-  },
-  keys = {
     {
       "<leader>?",
       function()
