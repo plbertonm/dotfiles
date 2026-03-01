fbce5cc53de4f18767581b09f1da74804d4ae469
plbertonm <plbertonm@gmail.com>
2026-02-28 22:31:21 -0300

    Removidos arquivos duplicados do neovim

--- lua/plugins/snippets.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "echasnovski/mini.snippets",
-  config = function()
-    local gen_loader = require("mini.snippets").gen_loader
-    require("mini.snippets").setup({})
-  end,
-}
--- lua/plugins/splitjoin.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "echasnovski/mini.splitjoin",
-  config = function ()
-    require('mini.splitjoin').setup({
-      mappings = { toggle = 'gS' }
-    })
-  end
-}
